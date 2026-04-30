1166b08437411e94bfbf9495acdd7f5796e35fab
jijiduc <jijiduc23@gmail.com>
2026-04-30 11:58:30 +0200

    feat : add TimeKeeper Agent

---
+++ TimeKeeper/agent.py
@@ -0,0 +1,50 @@
+import logging
+
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
+
+from .coordinator import TimeKeeperCoordinator
+
+logger = logging.getLogger("TimeKeeperAgent")
+
+
+class TimeKeeperAgent(Agent):
+    """TimeKeeper Spade Agent
+    """
+    def __init__(self, jid: str, password: str, verify_security: bool = False):
+        """Initialize the TimeKeeper SPADE agent.
+
+            Args:
+                jid (str): Jabber ID (XMPP address) of this agent
+                password (str): XMPP account password for authentication.
+                verify_security (bool, optional): Whether to enforce TLS certificate verification. Defaults to False.
+        """
+        super().__init__(jid, password, verify_security=verify_security)
+        self.coordinator = TimeKeeperCoordinator(send=self._send_message)
+
+    # SpadeRunner\src\common\camera_client.py
+    async def _send_message(self, recipient: str, body: str) -> None:
+        """Send message async to a specific recipient
+        """
+        msg = Message(to=recipient)
+        msg.body = body
+        await self.send(msg)
+
+    class CommandListener(CyclicBehaviour):
+
+        async def run(self):
+            msg = await self.receive(timeout=10)
+            if not msg:
+                return
+
+            sender = str(msg.sender)
+            body   = msg.body.strip()
+            logger.info(f"[TimeKeeper Agent] -- Recieved : '{body}' from {sender}")
+
+
+    async def setup(self) -> None:
+        """Spade agent life-cycle called once at agent start
+        """
+        logger.info(f"[TimeKeeper Agent] -- TimeKeeper start as {self.jid}")
+        self.add_behaviour(self.CommandListener())
