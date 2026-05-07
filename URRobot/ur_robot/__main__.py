767098997e4c7c68a4bcd4641e2d67e842e5e34b
caporizzi <capmars@gmail.com>
2026-05-07 16:01:37 +0200

    fix: arm robot working now

--- URRobot/ur_robot/__main__.py
+++ URRobot/ur_robot/__main__.py
@@ -39,6 +39,7 @@ async def start_ur_agent(run_agent: bool) -> Agent:
             logger.info("URAgent started successfully!")
         else:
             logger.info("URAgent created, but will not run!")
+        return test_agent
 
     except Exception as e:
         logger.error(f"Error starting agent: {str(e)}", exc_info=True)
@@ -47,8 +48,8 @@ async def start_ur_agent(run_agent: bool) -> Agent:
 from ur_robot.TESTURAgent import TESTURAgent
 async def start_test_ur_agent(run_agent: bool) -> Agent:
     # Read XMPP credentials and configuration from environment variables
-    xmpp_domain = os.environ.get("XMPP_DOMAIN")
-    xmpp_jid = f"test@isc-coordinator.lan"
+    xmpp_domain = os.environ.get("XMPP_DOMAIN", "isc-coordinator2.lan")
+    xmpp_jid = f"test@{xmpp_domain}"
     xmpp_password = os.environ.get("XMPP_PASSWORD", "top_secret")
         
     # Log the configuration for debugging purposes (masking the password)
@@ -70,6 +71,7 @@ async def start_test_ur_agent(run_agent: bool) -> Agent:
             logger.info("URAgent started successfully!")
         else:
             logger.info("URAgent created, but will not run!")
+        return test_agent
 
     except Exception as e:
         logger.error(f"Error starting agent: {str(e)}", exc_info=True)
@@ -78,9 +80,11 @@ async def start_test_ur_agent(run_agent: bool) -> Agent:
 
 async def main():
     ur_agent = await start_ur_agent(True)
-    test_ur_agent = await start_test_ur_agent(True)
+    test_ur_agent = None
+    if os.environ.get("RUN_TEST_UR_AGENT", "0") == "1":
+        test_ur_agent = await start_test_ur_agent(True)
 
-    if not(ur_agent is None or test_ur_agent is None):
+    if ur_agent is None:
         logger.error("One or more agents failed to start. Exiting.")
         return
 
@@ -105,7 +109,8 @@ async def main():
     finally:
         logger.info("Stopping agents...")
         await ur_agent.stop()
-        await test_ur_agent.stop()
+        if test_ur_agent:
+            await test_ur_agent.stop()
         logger.info("All agents stopped cleanly")
         
 if __name__ == "__main__":
